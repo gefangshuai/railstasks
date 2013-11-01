f85bf63741da87be96aa7ce0255d4c14bfcd3e26
gefangshuai <gefangshuai@163.com>
2013-11-01 10:49:51 +0800

    init project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TaskRails::Application.config.secret_key_base = '73bdefc8fd9bfdd2d3ce7edee7d96aa73a932c7c49a15dcad83d4f2ae22a33624f27edd50a4b50ca36e6ff64e5c8dbab7ea3542b0639365ebd44b2d61e95d24e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TaskRails::Application.config.session_store :cookie_store, key: '_task-rails_session'
